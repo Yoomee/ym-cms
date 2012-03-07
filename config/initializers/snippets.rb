6ea8a8cc37b561a57dedc457721e8abd8167331a
Ian Mooney <ian@yoomee.com>
2012-03-07 17:38:01 +0000

    Fixes for snippets

--- config/initializers/snippets.rb
+++ config/initializers/snippets.rb
@@ -4,12 +4,23 @@ class ActiveRecord::Base
 
     def has_snippets(*snippet_names)
       has_many :snippets, :as => :item, :dependent => :destroy, :autosave => true
+      accepts_nested_attributes_for :snippets, :allow_destroy => true, :reject_if => :all_blank
       snippet_names.each do |snippet_name|
         define_method snippet_name do
-          snippets.find_by_name(snippet_name.to_s).try(:text)
+          snippets.detect{|s| s.name == snippet_name.to_s}.try(:text)
         end
         define_method "#{snippet_name}=" do |val|
-          snippets.find_or_initialize_by_name(snippet_name.to_s).update_attributes(:text => val)
+          if existing_snippet = snippets.detect{|s| s.name == snippet_name.to_s}
+            if val.present?
+              existing_snippet.reload
+              existing_snippet.text = val    
+            else
+              existing_snippet.mark_for_destruction
+            end
+          elsif val.present?
+            self.snippets_attributes = [{:text => val, :name => snippet_name.to_s}]
+          end
+          val
         end
       end
     end
