9a3db3879f5af4d9d58dc3e40afc2863d9c98281
Ian Mooney <ian@yoomee.com>
2012-03-07 12:07:12 +0000

    Allow blank slug on page validations

--- lib/ym_cms/models/page.rb
+++ lib/ym_cms/models/page.rb
@@ -2,7 +2,7 @@ module YmCms::Page
   
   def self.included(base)
     base.validates :title, :presence => true
-    base.validates :slug, :uniqueness => true
+    base.validates :slug, :uniqueness => true, :allow_blank => true
     base.belongs_to :parent, :class_name => "Page"
     base.has_many :children, :class_name => "Page", :foreign_key => 'parent_id'
     base.validate :parent_is_not_self_or_child
