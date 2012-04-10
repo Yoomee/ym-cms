36ae617d53a54fdf4dbb277fe3de8cadf0b76340
Ian Mooney <ian@yoomee.com>
2012-04-10 12:18:21 +0100

    Moved slideshow migrations to seperate generator

--- lib/generators/ym_cms/install_generator.rb
+++ lib/generators/ym_cms/install_generator.rb
@@ -1,10 +1,12 @@
 module YmCms
   module Generators
+    
     class InstallGenerator < Rails::Generators::Base
       include Rails::Generators::Migration
-
+      include YmCms::NextMigrationNumber
+      
       source_root File.expand_path("../templates", __FILE__)
-      desc "Copies in migrations."
+      desc "Copies in default migrations."
 
       def self.next_migration_number(path)
         unless @prev_migration_nr
@@ -18,9 +20,29 @@ module YmCms
       def copy_migrations
         migration_template "create_pages.rb", "db/migrate/create_pages"
         migration_template "create_snippets.rb", "db/migrate/create_snippets"
+      end
+    end
+    
+    class SlideshowsGenerator < Rails::Generators::Base
+      include Rails::Generators::Migration
+
+      source_root File.expand_path("../templates", __FILE__)
+      desc "Copies in slideshow migrations."
+
+      def self.next_migration_number(path)
+        unless @prev_migration_nr
+          @prev_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
+        else
+          @prev_migration_nr += 1
+        end
+        @prev_migration_nr.to_s
+      end
+
+      def copy_migrations
         migration_template "create_slideshows.rb", "db/migrate/create_slideshows"
         migration_template "create_slides.rb", "db/migrate/create_slides"
       end
     end
+
   end
 end
