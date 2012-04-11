81256e05039c5c02141d42ca6aa6e9354a3d3846
Ian Mooney <ian@yoomee.com>
2012-04-11 12:29:13 +0100

    Only insert Page stuff in ability.rb if esists

--- lib/generators/ym_cms/install_generator.rb
+++ lib/generators/ym_cms/install_generator.rb
@@ -10,8 +10,10 @@ module YmCms
         copy_file "models/page.rb", "app/models/page.rb"
         copy_file "models/snippet.rb", "app/models/snippet.rb"
         copy_file "controllers/pages_controller.rb", "app/controllers/pages_controller.rb"
-        insert_into_file "app/models/ability.rb", "      can :show, Page, :published => true\n", :after => "can :manage, User, :id => user.id\n"
-        insert_into_file "app/models/ability.rb", "      can :show, Page, :published => true\n      cannot [:mercury_update], Page\n", :after => "cannot [:create, :update, :destroy], :all\n"
+        if File.exists?("#{Rails.root}/app/models/ability.rb")
+          insert_into_file "app/models/ability.rb", "      can :show, Page, :published => true\n", :after => "can :manage, User, :id => user.id\n"
+          insert_into_file "app/models/ability.rb", "      can :show, Page, :published => true\n      cannot [:mercury_update], Page\n", :after => "cannot [:create, :update, :destroy], :all\n"
+        end
         
         try_migration_template "migrations/create_pages.rb", "db/migrate/create_pages"
         try_migration_template "migrations/add_short_title_to_pages.rb", "db/migrate/add_short_title_to_pages"
