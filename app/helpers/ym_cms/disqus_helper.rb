2527f15b52a279c9f35248864c45af4bcdd5e521
Si Wilkins <si@yoomee.com>
2012-04-23 16:53:40 +0100

    Added auth handling

--- app/helpers/ym_cms/disqus_helper.rb
+++ app/helpers/ym_cms/disqus_helper.rb
@@ -5,7 +5,7 @@ module YmCms::DisqusHelper
     disqus_js =<<-JAVASCRIPT
     var disqus_shortname = '#{disqus_shortname(shortname)}'; // required: replace example with your forum shortname
     var disqus_identifier = '#{disqus_id(object)}';
-    var disqus_developer = #{Rails.env == 'development' ? 1 : 0};
+    var disqus_developer = #{(Rails.env == 'development' || http_basic_authenticated?) ? 1 : 0};
     (function() {
       var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
       dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
@@ -19,7 +19,7 @@ module YmCms::DisqusHelper
   def disqus_counts
     disqus_js =<<-JAVASCRIPT
     var disqus_shortname = '#{disqus_shortname(shortname)}'; // required: replace example with your forum shortname
-    var disqus_developer = #{Rails.env =='development' ? 1 : 0};
+    var disqus_developer = #{(Rails.env =='development' || http_basic_authenticated?) ? 1 : 0};
 
     /* * * DON'T EDIT BELOW THIS LINE * * */
     (function () {
@@ -43,6 +43,11 @@ module YmCms::DisqusHelper
   
   def disqus_shortname(shortname)
     Rails.env == 'production' ? shortname : 'yoomeedev'
+  end
+
+  private
+  def http_basic_authenticated?
+    !Settings.http_basic_authentication.blank?
   end
 
 end
