6329aa89ea7192c1cb51ea7c03a3a51923bfcf80
Si Wilkins <si@yoomee.com>
2012-04-23 16:29:13 +0100

    Made disuqs help configurable by client

--- app/helpers/ym_cms/disqus_helper.rb
+++ app/helpers/ym_cms/disqus_helper.rb
@@ -1,9 +1,9 @@
 module YmCms::DisqusHelper
 
-  def disqus_thread(object)
+  def disqus_thread(object, shortname)
     out = content_tag(:div, nil, :id => 'disqus_thread')
     disqus_js =<<-JAVASCRIPT
-    var disqus_shortname = '#{disqus_shortname}'; // required: replace example with your forum shortname
+    var disqus_shortname = '#{disqus_shortname(shortname)}'; // required: replace example with your forum shortname
     var disqus_identifier = '#{disqus_id(object)}';
     var disqus_developer = #{(Rails.env == 'development' || http_basic_authenticated?) ? 1 : 0};
     (function() {
@@ -18,14 +18,14 @@ module YmCms::DisqusHelper
   
   def disqus_counts
     disqus_js =<<-JAVASCRIPT
-    var disqus_shortname = '#{disqus_shortname}'; // required: replace example with your forum shortname
+    var disqus_shortname = '#{disqus_shortname(shortname)}'; // required: replace example with your forum shortname
     var disqus_developer = #{(Rails.env =='development' || http_basic_authenticated?) ? 1 : 0};
 
     /* * * DON'T EDIT BELOW THIS LINE * * */
     (function () {
         var s = document.createElement('script'); s.async = true;
         s.type = 'text/javascript';
-        s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
+        s.src = 'http://' + disqus_shortname(shortname) + '.disqus.com/count.js';
         (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
     }());
     JAVASCRIPT
@@ -33,7 +33,7 @@ module YmCms::DisqusHelper
   end
   
   def disqus_latest
-    url = "http://#{disqus_shortname}.disqus.com/recent_comments_widget.js?num_items=3&hide_avatars=1&avatar_size=32&excerpt_length=200"
+    url = "http://#{disqus_shortname(shortname)}.disqus.com/recent_comments_widget.js?num_items=3&hide_avatars=1&avatar_size=32&excerpt_length=200"
     javascript_include_tag(url)
   end 
   
@@ -41,11 +41,8 @@ module YmCms::DisqusHelper
     "#{Rails.env =='development' ? 'Dev' : ''}#{object.class.to_s}#{object.id}"
   end
   
-  def disqus_shortname
-    case Rails.env
-    when "production" then "yoomee3"
-    else "yoomeedev"
-    end
+  def disqus_shortname(shortname)
+    Rails.env == 'production' ? shortname : 'yoomeedev'
   end
 
 end
