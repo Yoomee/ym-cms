fd4738ae3b536dedbbbd39f7854342d5fafcb8f5
Si Wilkins <si@yoomee.com>
2012-04-23 16:41:58 +0100

    Removed auth check

--- app/helpers/ym_cms/disqus_helper.rb
+++ app/helpers/ym_cms/disqus_helper.rb
@@ -5,7 +5,7 @@ module YmCms::DisqusHelper
     disqus_js =<<-JAVASCRIPT
     var disqus_shortname = '#{disqus_shortname(shortname)}'; // required: replace example with your forum shortname
     var disqus_identifier = '#{disqus_id(object)}';
-    var disqus_developer = #{(Rails.env == 'development' || http_basic_authenticated?) ? 1 : 0};
+    var disqus_developer = #{Rails.env == 'development' ? 1 : 0};
     (function() {
       var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
       dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
@@ -19,7 +19,7 @@ module YmCms::DisqusHelper
   def disqus_counts
     disqus_js =<<-JAVASCRIPT
     var disqus_shortname = '#{disqus_shortname(shortname)}'; // required: replace example with your forum shortname
-    var disqus_developer = #{(Rails.env =='development' || http_basic_authenticated?) ? 1 : 0};
+    var disqus_developer = #{Rails.env =='development' ? 1 : 0};
 
     /* * * DON'T EDIT BELOW THIS LINE * * */
     (function () {
