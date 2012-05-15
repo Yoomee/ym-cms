15540b34c39556b11be47dbf55342932a28eb251
Ian Mooney <ian@yoomee.com>
2012-05-15 09:34:59 +0100

    Fixed slide test

--- spec/factories.rb
+++ spec/factories.rb
@@ -17,6 +17,7 @@ FactoryGirl.define do
     f.after_build do |slide|
       image_mock = f.double('image_mock')
       image_mock.stub(:size => 100.kilobytes)
+      image_mock.stub(:format => :png)      
       image_mock.stub(:mime_type => 'image/png')
       image_mock.stub(:url => '/url/to/image.png')
       image_mock.stub(:process => image_mock)
