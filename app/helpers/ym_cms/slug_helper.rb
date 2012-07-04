bb0a7ebd35a4583b7b28b98dd8bb745b04b3011a
Matt Atkins <matt@yoomee.com>
2012-07-04 17:17:43 +0100

    Remove decent_exposure

--- app/helpers/ym_cms/slug_helper.rb
+++ app/helpers/ym_cms/slug_helper.rb
@@ -1,11 +1,11 @@
 module YmCms::SlugHelper
   
   def slug_is?(slug)
-    defined?(page) && page.slug == slug.to_s
+    @page && @page.slug == slug.to_s
   end
   
   def root_slug_is?(slug)
-    defined?(page) && page.root_slug == slug.to_s
+    @page && @page.root_slug == slug.to_s
   end
   
 end
--- lib/ym_cms/controllers/pages_controller.rb
+++ lib/ym_cms/controllers/pages_controller.rb
@@ -1,44 +1,44 @@
 module YmCms::PagesController
   
   def self.included(base)
-    base.expose(:page)
-    base.expose(:page_children) { page ? page.children : [] }
-    base.expose(:pages_for_ordering) { params[:id] ? page_children : Page.root}
+    base.load_and_authorize_resource
     base.send(:include, YmPermalinks::PermalinkableController)
   end
   
   def create
     set_user
-    if page.save
-      flash_notice(page)
-      redirect_to page
+    if @page.save
+      flash_notice(@page)
+      redirect_to @page
     else
       render :action => "new"
     end
   end
   
   def destroy
-    page.destroy
-    flash_notice(page)
-    redirect_to page.parent || pages_path
+    @page.destroy
+    flash_notice(@page)
+    redirect_to @page.parent || pages_path
   end
   
   def new
-    page.parent_id = params[:parent_id]
     set_user
   end
   
   def order
+    @page = Page.find_by_id(params[:id])
+    @pages_for_ordering = @page.present? ? (@page.children || []) : Page.root
   end
   
   def show
-    render :action => "views/#{page.view_name}"
+    @page_children = @page.children || []
+    render :action => "views/#{@page.view_name}"
   end
   
   def update
-    if page.save
-      flash_notice(page)
-      redirect_to page
+    if @page.update_attributes(params[:page])
+      flash_notice(@page)
+      redirect_to @page
     else
       render :action => "edit"
     end
@@ -54,7 +54,7 @@ module YmCms::PagesController
   
   private
   def set_user
-    page.user = current_user if page.user.nil? && defined?(current_user)
+    @page.user = current_user if @page.user.nil? && defined?(current_user)
   end
   
 end
