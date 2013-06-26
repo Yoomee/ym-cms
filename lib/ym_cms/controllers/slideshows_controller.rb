47d2c001eb8b0a1ce8d9674bfaf74d49a41e0351
Chrystal Griffiths <chrystalgriffiths@Chrystal-Griffithss-iMac.local>
2013-06-26 11:48:39 +0100

    Allow independent slideshows

--- lib/ym_cms/controllers/slideshows_controller.rb
+++ lib/ym_cms/controllers/slideshows_controller.rb
@@ -1,13 +1,37 @@
 module YmCms::SlideshowsController
   
   def self.included(base)
-    base.expose(:slideshow)
-    base.prepend_before_filter(:unnest_params, :only => [:update])
+    base.load_and_authorize_resource
+    base.prepend_before_filter(:unnest_params, :only => [:create, :update])
   end
+
+  def edit
+    @slideshow.independent_slideshow = true
+  end
+
+  def index
+    @slideshows = Slideshow.where('slug IS NOT NULL')
+  end
+
+  def new
+    @slideshow.independent_slideshow = true
+  end
+
+  def create
+    if @slideshow.save
+      flash_notice(@slideshow)
+      redirect_to @slideshow
+    else
+      render :action => "new"
+    end  
+  end
+
+  def show
+  end  
   
   def update
-    if slideshow.save
-      redirect_to root_path
+    if @slideshow.update_attributes(params[:slideshow])
+      redirect_to @slideshow
     else
       render :action => "edit"
     end
