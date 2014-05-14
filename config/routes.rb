b1d980d7a556b3994463b329863b8cdcafd2fc12
Matt Atkins <matt@yoomee.com>
2014-05-14 12:18:46 +0100

    Fix routes for Rails 4

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   end
   match "pages/order", :to => "pages#update_order", :via => :post 
   match "pages/:id/order", :to => "pages#update_order", :via => :post 
-  match "pages/:parent_id/subpage", :to => 'pages#new', :as => "new_child_page"
+  get "pages/:parent_id/subpage", :to => 'pages#new', :as => "new_child_page"
 
 
   
